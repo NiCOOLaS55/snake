ca80bb75c03c14e2ca73ba9aa0832f018881bd80
NiCOOLaS55 <pkrnl2012@gmail.com>
2019-03-15 15:24:21 +0300

    HorizontalLines

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,28 +16,7 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(3, 2, '№');
-            p3.Draw();
-
-            Point p4 = new Point(5, 1, '$');
-            p4.Draw();
-
-            List<char> symlist = new List<char>
-            {
-                '*',
-                '#',
-                '№',
-                '$'
-            };
-
-            List<Point> plist = new List<Point>
-            {
-                p1,
-                p2,
-                p3,
-                p4
-            };
-
+           /*sdsg*/
 
             Console.ReadLine();
         }

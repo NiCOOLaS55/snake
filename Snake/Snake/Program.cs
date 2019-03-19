c7bf563f80510abe84c6717a851da365f145d646
NiCOOLaS55 <pkrnl2012@gmail.com>
2019-03-19 18:47:52 +0300

    создаем класс стены и обработчик коллизий

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,17 +12,11 @@ namespace Snake
         {
              /* устанавливаем размер окна консоли*/
             Console.SetBufferSize( 80, 25 );
-            /* рисуем рамку окна консоли*/
 
-            HorizontalLines HUpline = new HorizontalLines(0, 78, 0, '+');
-            HorizontalLines HDownline = new HorizontalLines(0, 78, 24, '+');
-            VerticalLines VLeftline = new VerticalLines(0, 24, 0, '+');
-            VerticalLines VRightline = new VerticalLines(0, 24, 78, '+');
-            HUpline.Draw();
-            HDownline.Draw();
-            VLeftline.Draw();
-            VRightline.Draw();
-
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+            
+            
             /* отрисовка точек*/
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
@@ -34,6 +28,11 @@ namespace Snake
 
             while(true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if(snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        List<Figure> wallist;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallist = new List<Figure>();
+            
+            /* рисуем рамку окна консоли*/
+            HorizontalLines HUpline = new HorizontalLines(0, mapWidth - 2, 0, '+');
+            HorizontalLines HDownline = new HorizontalLines(0, mapWidth - 2, mapHeight - 1, '+');
+            VerticalLines VLeftline = new VerticalLines(0, mapHeight - 1, 0, '+');
+            VerticalLines VRightline = new VerticalLines(0, mapHeight - 1, mapWidth - 2, '+');
+
+            wallist.Add(HUpline);
+            wallist.Add(HDownline);
+            wallist.Add(VLeftline);
+            wallist.Add(VRightline);            
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallist)
+            {
+                if(wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallist)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
